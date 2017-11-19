e9cac6662bfe34cb008185babec51fdcbd16b9ad
Alex Belokur <alex.belokur17@gmail.com>
2017-11-19 20:33:06 +0200

    board for statistic

---
+++ js/stat.js
@@ -0,0 +1,11 @@
+'use strict'
+
+window.renderStatistics = function (ctx, names, times) {
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 10, 420, 270);
+  console.log('hello')
+}
