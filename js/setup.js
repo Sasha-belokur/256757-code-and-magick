89248c64f08aca3189b199fcba65f31ad9107073
sasha-belokur <alex.belokur17@gmail.com>
2017-11-27 11:20:58 +0200

    removed wizardDataTemplate and array wizards from global scope, created separate function to show userDialog

--- js/setup.js
+++ js/setup.js
@@ -1,21 +1,16 @@
 'use strict';
-var wizardDataTemplate = {
-  names: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-  surnames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-  coatColors: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-  eyesColors: ['black', 'red', 'blue', 'yellow', 'green']
-};
-var wizards = [];
-var userDialog = document.querySelector('.setup');
-
-userDialog.classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
-
 var getRandomNum = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
 var Wizard = function () {
+  var wizardDataTemplate = {
+    names: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+    surnames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+    coatColors: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+    eyesColors: ['black', 'red', 'blue', 'yellow', 'green']
+  };
+
   this.name = wizardDataTemplate.names[getRandomNum(0, wizardDataTemplate.names.length - 1)];
   this.surname = wizardDataTemplate.surnames[getRandomNum(0, wizardDataTemplate.surnames.length - 1)];
   this.coatColor = wizardDataTemplate.coatColors[getRandomNum(0, wizardDataTemplate.coatColors.length - 1)];
@@ -23,12 +18,14 @@ var Wizard = function () {
 };
 
 var createWizards = function (amount) {
+  var wizards = [];
+
   for (var i = 0; i < amount; i++) {
     wizards.push(new Wizard());
   }
-};
 
-createWizards(4);
+  return wizards;
+};
 
 var createWizardElement = function (wizard) {
   var wizardElementTemplate = document.querySelector('#similar-wizard-template').content;
@@ -42,6 +39,8 @@ var createWizardElement = function (wizard) {
 };
 
 var renderWizards = function () {
+
+  var wizards = createWizards(4);
   var fragment = document.createDocumentFragment();
   var similarListElement = document.querySelector('.setup-similar-list');
 
@@ -52,4 +51,12 @@ var renderWizards = function () {
   similarListElement.appendChild(fragment);
 };
 
-renderWizards();
+var openUserDialog = function () {
+  var userDialog = document.querySelector('.setup');
+  userDialog.classList.remove('hidden');
+  document.querySelector('.setup-similar').classList.remove('hidden');
+
+  renderWizards();
+};
+
+openUserDialog();
